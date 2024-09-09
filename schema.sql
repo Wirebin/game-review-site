ca4603708ece40436819ee397eba2f86ff9c0bef
Wirebin <lassi.lahtinen02@gmail.com>
2024-09-10 02:35:01 +0300

    Add database schema.sql

---
+++ schema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username TEXT UNIQUE,
+	password TEXT,
+	access_level TEXT
+);
+
+CREATE TABLE games (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	release_date DATE,
+	created_at TIMESTAMP
+);
+
+CREATE TABLE genres (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE
+);
+
+CREATE TABLE game_genres (
+	id SERIAL PRIMARY KEY,
+	game_id INTEGER REFERENCES games,
+	genre_id INTEGER REFERENCES genres
+);
+
+CREATE TABLE scores (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	game_id INTEGER REFERENCES games,
+	score INTEGER
+);
+
+CREATE TABLE reviews (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	game_id INTEGER REFERENCES games,
+	content TEXT,
+	created_at TIMESTAMP
+);
+
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	game_id INTEGER REFERENCES games,
+	title TEXT,
+	content TEXT,
+	created_at TIMESTAMP
+);
+
+CREATE TABLE replies (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	game_id INTEGER REFERENCES games,
+	post_id INTEGER REFERENCES posts,
+	reply_count INTEGER,
+	content TEXT,
+	created_at TIMESTAMP
+);
+
+
+
+
