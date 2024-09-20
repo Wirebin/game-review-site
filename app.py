7c9b0dd1cf61521c8ba024882cb020e08c4eccc6
Wirebin <lassi.lahtinen02@gmail.com>
2024-09-20 23:25:39 +0300

    Update session cookie security

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_HTTPONLY"] = True
 
 db = SQLAlchemy(app)
 
