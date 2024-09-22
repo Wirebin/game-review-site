59c88c4bcef10a49f7552880945f4f4c1e44b543
Wirebin <lassi.lahtinen02@gmail.com>
2024-09-23 01:14:24 +0300

    Add Post and Review Models

--- models.py
+++ models.py
@@ -7,3 +7,26 @@ class Game(db.Model):
     release_date = db.Column(db.Date, nullable=False)
 
 
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    game_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def formatted_date(self):
+        return self.created_at.strftime("%H:%M - %d.%m.%Y")
+
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    game_id = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.Date, nullable=False)
+
+    def formatted_date(self):
+        return self.created_at.strftime("%H:%M - %d.%m.%Y")
