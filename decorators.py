c82ea38c1a754a4638e688b1a4e32b1c5114b98e
Wirebin <lassi.lahtinen02@gmail.com>
2024-09-23 00:29:13 +0300

    Add login_required decorator

--- decorators.py
+++ decorators.py
@@ -1,14 +1,20 @@
-from flask import redirect, url_for, session, abort
+from flask import redirect, session, abort, url_for
 from functools import wraps
 
 
 def admin_required(f):
     @wraps(f)
     def check_access(*args, **kwargs):
-        print("Decorator called")
-        print("Session access level:", session.get('access_level'))
         if session.get('access_level') != 'admin':
             return abort(404)
         return f(*args, **kwargs)
     return check_access
 
+
+def login_required(f):
+    @wraps(f)
+    def check_login(*args, **kwargs):
+        if not session.get('user_id'):
+            return redirect(url_for("login"))
+        return f(*args, **kwargs)
+    return check_login
