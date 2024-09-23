8fd53cbc331f12ae92a0c724af486427ea9d6951
Wirebin <lassi.lahtinen02@gmail.com>
2024-09-23 15:30:56 +0300

    Add Reply and User Models

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
 from app import db
 
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+
+
 class Game(db.Model):
     __tablename__ = "games"
     id = db.Column(db.Integer, primary_key=True)
@@ -10,12 +17,32 @@ class Game(db.Model):
 class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    game_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=False)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
+    user = db.relationship("User", backref=db.backref("replies", lazy=True))
+    game = db.relationship("Game", backref=db.backref("replies", lazy=True))
+
+    def formatted_date(self):
+        return self.created_at.strftime("%H:%M - %d.%m.%Y")
+
+
+class Reply(db.Model):
+    __tablename__ = "replies"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship("User", backref=db.backref("user_replies", lazy=True))
+    game = db.relationship("Game", backref=db.backref("game_replies", lazy=True))
+    post = db.relationship("Post", backref=db.backref("post_replies", lazy=True))
+    
     def formatted_date(self):
         return self.created_at.strftime("%H:%M - %d.%m.%Y")
 
@@ -23,10 +50,11 @@ class Post(db.Model):
 class Review(db.Model):
     __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    game_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.Date, nullable=False)
+
 
     def formatted_date(self):
         return self.created_at.strftime("%H:%M - %d.%m.%Y")
