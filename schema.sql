60bb6dc8c410b99ebf392e64ce70c8d35bd295a6
Wirebin <lassi.lahtinen02@gmail.com>
2024-10-06 22:35:28 +0300

    Update 'games' and reviews' tables

--- schema.sql
+++ schema.sql
@@ -9,6 +9,8 @@ CREATE TABLE games (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
 	description TEXT,
+	developer TEXT,
+	publisher TEXT,
 	release_date DATE,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -35,7 +37,9 @@ CREATE TABLE reviews (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users,
 	game_id INTEGER REFERENCES games,
+	title TEXT,
 	content TEXT,
+	score INTEGER,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
