d41b520837ba56b1dc1663d32727ef5df62d74c8
Wirebin <lassi.lahtinen02@gmail.com>
2024-10-20 23:57:21 +0300

    Change the way access_level is checked

--- decorators.py
+++ decorators.py
@@ -1,11 +1,13 @@
 from flask import redirect, session, abort, url_for
+from account import get_user_by_id
 from functools import wraps
 
 
 def admin_required(f):
     @wraps(f)
     def check_access(*args, **kwargs):
-        if session.get('access_level') != 'admin':
+        user = get_user_by_id(session.get("user_id"))
+        if user.access_level != "admin":
             return abort(404)
         return f(*args, **kwargs)
     return check_access
